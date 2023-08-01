047ca660e498ac66b95e31e6592f39d0dedd88b1
aledex <116499782+AledexOG@users.noreply.github.com>
2023-08-01 13:54:31 -0400

    Add files via upload

---
+++ Credits.bat
@@ -0,0 +1,22 @@
+@echo off
+cls
+title Creds
+mode 70,25
+color a
+echo.
+
+echo.
+echo -------------------------------------------------------------
+echo -    Aledex is the owner, no staffs, no admins              -
+echo -------------------------------------------------------------
+set /p Owners=
+(IPpingerV1.exe)
+goto fin
+echo              _      _       _____   ____    _____  __  __        
+echo             / \    | |     | ____| |  _ \  | ____| \ \/ /        
+echo            / _ \   | |     |  _|   | | | | |  _|    \  /         
+echo           / ___ \  | |___  | |___  | |_| | | |___   /  \         
+echo   _____  /_/   \_\ |_____| |_____| |____/  |_____| /_/\_\  _____ 
+echo  |_____|                                                  |_____|
+
+:fin EXIT /B
---
+++ README.bat
@@ -0,0 +1,15 @@
+@echo off
+cls
+title README.
+mode 70,30
+color 2
+echo.
+
+echo.
+echo why do you need to run as admin
+echo 1. so the thing can ping your ip
+echo ----------------------------------------------------------------
+echo how to use
+echo 1. Open IPpingerV1 or type something random and it wil open here
+set /p HTU=
+(IPpingerV1.exe)
